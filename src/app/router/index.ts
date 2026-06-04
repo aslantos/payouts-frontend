cf8029393bb0a8c40151febce1ed69d76de00abe
Aslantos <aslantb007@gmail.com>
2026-06-04 13:17:53 +0500

    Добавлены страницы-заглушки и маршруты всех экранов

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,10 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/pages/login/LoginPage.vue'
 import DashboardPage from '@/pages/dashboard/DashboardPage.vue'
+import PayoutDetailPage from '@/pages/payout-detail/PayoutDetailPage.vue'
+import PayoutsPage from '@/pages/payouts/PayoutsPage.vue'
+import TariffsPage from '@/pages/tariffs/TariffsPage.vue'
+import UsersPage from '@/pages/users/UsersPage.vue'
+import WorkLogNewPage from '@/pages/work-log-new/WorkLogNewPage.vue'
+import WorkLogsPage from '@/pages/work-logs/WorkLogsPage.vue'
+import WorkTypesPage from '@/pages/work-types/WorkTypesPage.vue'
 
 const routes = [
   { path: '/login', component: LoginPage },
   { path: '/dashboard', component: DashboardPage },
+  { path: '/payouts/:id', component: PayoutDetailPage },
+  { path: '/payouts', component: PayoutsPage },
+  { path: '/tariffs', component: TariffsPage },
+  { path: '/users', component: UsersPage },
+  { path: '/work-logs', component: WorkLogsPage },
+  { path: '/work-logs/new', component: WorkLogNewPage},
+  { path: '/work-types', component: WorkTypesPage },
 ]
 
 export const router = createRouter({
---
+++ src/pages/work-logs/WorkLogsPage.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="p-8 text-2xl font-bold">
+    WorkLogsPage
+  </div>
+</template>
